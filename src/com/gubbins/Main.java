fb99edd46e5c46116eff4274902cb3d4d0135814
Sam Marshall <sm658@kent.ac.uk>
2015-02-27 11:24:22 +0000

    Added charting library and example

--- src/com/gubbins/Main.java
+++ src/com/gubbins/Main.java
@@ -1,8 +1,19 @@
 package com.gubbins;
 
+import com.xeiam.xchart.Chart;
+import com.xeiam.xchart.QuickChart;
+import com.xeiam.xchart.SwingWrapper;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double[] xData = new double[] { 0.0, 1.0, 2.0 };
+        double[] yData = new double[] { 2.0, 1.0, 0.0 };
+
+        // Create Chart
+        Chart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+
+        // Show it
+        new SwingWrapper(chart).displayChart();
     }
 }
