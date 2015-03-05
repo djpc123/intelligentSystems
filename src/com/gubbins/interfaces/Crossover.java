56f0002bb36335274d42e84e4fe83b587a29361e
retooks <sm658@kent.ac.uk>
2015-03-05 13:00:47 +0000

    Crossover should return two children

--- src/com/gubbins/interfaces/Crossover.java
+++ src/com/gubbins/interfaces/Crossover.java
@@ -2,13 +2,18 @@ package com.gubbins.interfaces;
 
 import com.gubbins.Function;
 
+import java.util.ArrayList;
+
 /**
  * Created by Sam on 03/03/2015.
  */
 public interface Crossover {
 
-    public default Function crossover(Function f1, Function f2) {
-        return f1;
+    public default ArrayList<Function> crossover(Function f1, Function f2) {
+        ArrayList<Function> children = new ArrayList<>();
+        children.add(f1);
+        children.add(f2);
+        return children;
     }
 
 }
